ad665ba1197bb0ce4239a8bd58e3c6403b6e7552
MATÍAS ABOUDARA <47701750@ort>
2023-05-18 10:07:19 -0300

    Proyecto creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP04_Bis_Aboudara.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
